ef9a3350842e85c416492aaf603b9bd6a8c35e48
MichaelYuhe <2312744987@qq.com>
2022-07-02 10:27:25 +0800

    self description

--- components/Sections/About.section.tsx
+++ components/Sections/About.section.tsx
@@ -9,37 +9,16 @@ const About: NextComponentType = () => {
   return (
     <div className="my-8 flex flex-row items-center justify-between px-3 font-sen">
       <div>
-        <p className="text-3xl font-bold text-white">Anurag</p>
+        <p className="text-3xl font-bold text-white">MichaelYuhe</p>
         <p className="mt-1 text-lg text-gray-300">
-          Frontend Developer, Blogger and a Student
+          Frontend Developer, Student
         </p>
 
         <p className="mt-4 text-gray-400">
-          Building Web apps, proficient in Frontend. Writing articles <br /> on
-          my blog sometimes.
+          Start my Frontend Develop Jouney from 2021/7.<br />
+          Working at Netease currently. <br />
+          Graduating next year from ZJU.
         </p>
-
-        <Link href="https://blog.anurag.tech" passHref>
-          <a
-            className="mt-4 flex cursor-pointer flex-row items-center gap-1 font-jost text-xl text-gray-400 duration-100 hover:ml-2"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read my Blog
-            <HiOutlineArrowNarrowRight />
-          </a>
-        </Link>
-
-        <Link href="https://sponsor.anurag.tech" passHref>
-          <a
-            className="mt-4 flex cursor-pointer flex-row items-center gap-1 font-jost text-xl text-gray-400 duration-100 hover:ml-2"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Love my work? Sponsor me!
-            <HiOutlineArrowNarrowRight />
-          </a>
-        </Link>
       </div>
 
       <div className="hidden custom:block">
